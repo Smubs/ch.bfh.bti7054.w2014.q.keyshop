b8758af59ad16d33820fad05f5fa1a31cc02a97b
michaelraess <raess.michael@gmail.com>
2014-10-17 14:40:45 +0200

    Added jQuery, AngularJS and Bootstrap

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -3,5 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<title>Key Shop</title>
+
+    <script type="text/javascript" src="/assets/scripts/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="/assets/scripts/angular.min.js"></script>
+    <script type="text/javascript" src="/assets/scripts/ui-bootstrap-tpls-0.11.2.min.js"></script>
+
+    <link type="text/css" rel="stylesheet" href="/assets/styles/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="/assets/styles/bootstrap-theme.min.css" />
 </head>
 <body>
